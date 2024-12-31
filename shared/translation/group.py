d337509a129330921f47910eb3506b6d774d148d
Moskize91 <i@taozeyu.com>
2024-12-31 17:03:00 +0800

    feat(project): support new translation logic & complete flow (#2)

--- shared/translation/group.py
+++ shared/translation/group.py
@@ -1,35 +1,104 @@
 import tiktoken
 
-from typing import Iterable, Generator
+from dataclasses import dataclass
+from typing import Generator
+from .nlp import NLP
 
+
+@dataclass
+class Fragment:
+  id: int
+  origin: str
+  target: str
+  tokens: int
+  index: int
+  sentence_index: int
+  sentences: int
 
 class Group:
-  def __init__(self, group_max_tokens: int) -> None:
+  def __init__(self, group_max_tokens: int, interval_max_tokens: int) -> None:
     self._encoder: tiktoken.Encoding = tiktoken.get_encoding("o200k_base")
+    self._nlp: NLP = NLP("en")
     self._group_max_tokens: int = group_max_tokens
+    self._interval_max_tokens: int = interval_max_tokens
 
-  def split(self, texts: Iterable[str]) -> Generator[list[tuple[int, str]], None, None]:
-    text_pairs = [
-      (text, len(self._encoder.encode(text)))
-      for text in texts
-    ]
-    current_group: list[tuple[int, str]] = []
+  def split(self, texts: list[str]) -> list[list[Fragment]]:
+    matrix: list[list[Fragment]] = []
+    all_fragments = self._transform_to_fragments(texts)
+    for id, fragment in enumerate(all_fragments):
+      fragment.id = id
+    for fragments in self._split_fragments(all_fragments):
+      matrix.append(fragments)
+    return matrix
+
+  def _transform_to_fragments(self, texts: list[str]) -> list[Fragment]:
+    fragments: list[Fragment] = []
+    for index, text in enumerate(texts):
+      tokens: int = len(self._encoder.encode(text))
+      if tokens < self._group_max_tokens:
+        fragments.append(Fragment(
+          id=0,
+          origin=text,
+          target="",
+          tokens=tokens,
+          index=index,
+          sentence_index=0,
+          sentences=1,
+        ))
+      else:
+        for fragment in self._split_large_text(index, text):
+          fragments.append(fragment)
+    return fragments
+
+  def _split_large_text(self, index: int, text: str) -> Generator[Fragment, None, None]:
+    text_index_list: list[tuple[str, int]] = []
+    for sentence in self._nlp.split_into_sents(text):
+      text = sentence.text
+      tokens = self._encoder.encode(text)
+      while len(tokens) > self._group_max_tokens:
+        head_tokens = tokens[:self._group_max_tokens]
+        tokens = tokens[self._group_max_tokens:]
+        head_text = self._encoder.decode(head_tokens)
+        text_index_list.append((head_text, len(head_tokens)))
+      if len(tokens) > 0:
+        tail_text = self._encoder.decode(tokens)
+        text_index_list.append((tail_text, len(tokens)))
+
+    for sentence_index, (text, tokens) in enumerate(text_index_list):
+      yield Fragment(
+        id=0,
+        origin=text,
+        target="",
+        tokens=tokens,
+        index=index, 
+        sentence_index=sentence_index, 
+        sentences=len(text_index_list),
+      )
+
+  def _split_fragments(self, fragments: list[Fragment]) -> Generator[list[Fragment], None, None]:
+    current_group: list[Fragment] = []
     current_tokens: int = 0
-    last_tokens: int = 0
 
-    for index, (text, tokens) in enumerate(text_pairs):
-      if len(current_group) > 0 and current_tokens + tokens > self._group_max_tokens:
+    for fragment in fragments:
+      next_tokens: int = current_tokens + fragment.tokens
+      if len(current_group) > 0 and next_tokens > self._group_max_tokens:
         yield current_group
-        if tokens > self._group_max_tokens:
-          current_group = []
-          current_tokens = 0
-        else:
-          current_group = [current_group[-1]]
-          current_tokens = last_tokens
+        current_group, current_tokens = self._keep_interval(current_group)
 
-      current_group.append((index, text))
-      current_tokens += tokens
-      last_tokens = tokens
+      current_group.append(fragment)
+      current_tokens += fragment.tokens
 
-    if len(current_group) > 1:
+    if len(current_group) > 0:
       yield current_group
+
+  def _keep_interval(self, group: list[Fragment]):
+    keep_group: list[Fragment] = []
+    keep_tokens: int = 0
+    for fragment in reversed(group):
+      next_tokens = keep_tokens + fragment.tokens
+      if len(keep_group) > 0 and next_tokens > self._interval_max_tokens:
+        break
+      keep_tokens += fragment.tokens
+      keep_group.append(fragment)
+    keep_group.reverse()
+    return keep_group, keep_tokens
--- shared/translation/translater.py
+++ shared/translation/translater.py
@@ -1,7 +1,8 @@
 import re
+import math
 
 from typing import Callable
-from .group import Group
+from .group import Group, Fragment
 from .llm import LLM, LLM_API
 
 _lan_full_name: dict[str, str] = {
@@ -14,18 +15,21 @@ _lan_full_name: dict[str, str] = {
 
 class Translater:
   def __init__(
-      self,
-      group_max_tokens: int,
-      api: LLM_API, 
-      key: str | None, 
-      url: str | None, 
-      model: str,
-      temperature: float,
-      timeout: float | None,
-      source_lan: str | None,
-      target_lan: str,
-    ) -> None:
-    self._group: Group = Group(group_max_tokens)
+        self,
+        group_max_tokens: int,
+        api: LLM_API, 
+        key: str | None, 
+        url: str | None, 
+        model: str,
+        temperature: float,
+        timeout: float | None,
+        source_lan: str,
+        target_lan: str,
+      ) -> None:
+    self._group: Group = Group(
+      group_max_tokens=group_max_tokens, 
+      interval_max_tokens=math.ceil(float(group_max_tokens) * 0.1),
+    )
     self._llm = LLM(
       api=api,
       key=key,
@@ -35,44 +39,50 @@ class Translater:
       timeout=timeout,
     )
     self._admin_prompt: str = _gen_admin_prompt(
+      source_lan=self._lan_full_name(source_lan),
       target_lan=self._lan_full_name(target_lan),
-      source_lan=None if source_lan is None else self._lan_full_name(source_lan),
     )
 
   def translate(self, source_texts: list[str], report_progress: Callable[[float], None]) -> list[str]:
     target_texts: list[str] = [""] * len(source_texts)
-    max_index: int = 0
+    grouped_fragments = self._group.split(source_texts)
+    operated_id: int = 0
 
-    for chunk in self._group.split(source_texts):
-      chunk_texts: list[str] = []
-      chunk_indexes: list[int] = []
-      for index, text in chunk:
-        text = text.strip()
-        if text != "":
-          chunk_texts.append(text)
-          chunk_indexes.append(index)
-
-      if len(chunk_texts) > 0:
-        chunk_texts = self._translate_texts(chunk_texts)
-      
-      for index, text in zip(chunk_indexes, chunk_texts):
-        target_texts[index] = text
-        max_index = max(index, max_index)
-      
-      report_progress(float(max_index + 1) / float(len(source_texts)))
+    for fragments in grouped_fragments:
+      for fragment in self._translate_fragments(fragments):
+        if operated_id > fragment.id:
+          continue # fragment may be duplicated
+        operated_id = max(operated_id, fragment.id)
+        target_texts[fragment.index] += fragment.target
+      report_progress(float(operated_id + 1) / float(len(source_texts)))
 
     return target_texts
 
+  def _translate_fragments(self, fragments: list[Fragment]) -> list[Fragment]:
+    texts: list[str] = []
+    indexes: list[int] = []
+    for index, fragment in enumerate(fragments):
+      text = fragment.origin.strip()
+      if text != "":
+        texts.append(text)
+        indexes.append(index)
+    if len(texts) > 0:
+      texts = self._translate_texts(texts)
+    for index, text in zip(indexes, texts):
+      fragments[index].target = text
+    return fragments
+
   def _translate_texts(self, texts: list[str]) -> list[str]:
+    texts = [f"{i+1}: {t}" for i, t in enumerate(texts)]
     content = self._llm.invoke(
       system=self._admin_prompt,
       human="\n".join(texts),
     )
     target_texts: list[str] = []
     for line in content.split("\n"):
-      match = re.search(r"^\d+(\:|\.)", line)
+      match = re.search(r"^\d+\:", line)
       if match:
-        text = re.sub(r"^\d+(\:|\.)\s*", "", line)
+        text = re.sub(r"^\d+\:\s*", "", line)
         target_texts.append(text)
 
     return target_texts
@@ -83,14 +93,22 @@ class Translater:
       full_name = _lan_full_name["en"]
     return full_name
 
-def _gen_admin_prompt(target_lan: str, source_lan: str | None) -> str:
-  if source_lan is None:
-    source_lan = "any language and you will detect the language"
+def _gen_admin_prompt(target_lan: str, source_lan: str) -> str:
   return f"""
-I want you to act as an {target_lan} translator, spelling corrector and improver. 
-Next user will speak to you in {source_lan}, translate it and answer in the corrected and improved version of my text, in {target_lan}. 
-I want you to replace simplified A0-level words and sentences with more beautiful and elegant, upper level Chinese words and sentences. Keep the meaning same, but make them more literary. 
-I want you to only reply the correction, the improvements and nothing else, do not write explanations.
-Next user will speak a passage. The passage is divided into multiple lines, each line starting with a number (an Arabic numeral followed by a colon).
-Your translation should also respond in multiple lines, with corresponding numbers at the beginning of each line in the translation.
-  """
+You are a translator and need to translate the user's {source_lan} text into {target_lan}.
+I want you to replace simplified A0-level words and sentences with more beautiful and elegant, upper level {target_lan} words and sentences. Keep the meaning same, but make them more literary. 
+I want you to only reply the translation and nothing else, do not write explanations.
+A number and colon are added to the top of each line of text entered by the user. This number is only used to align the translation text for you and has no meaning in itself. You should delete the number in your mind to understand the user's original text.
+Your translation results should be split into a number of lines, the number of lines is equal to the number of lines in the user's original text. The content of each line should correspond to the corresponding line of the user's original text.
+The translated lines must not be missing, added, misplaced, or have their order changed. They must correspond exactly to the original text of the user.
+
+Here is an example. First, the user submits the original text in English (this is just an example):
+1: This true without lying, certain & most true:
+2: That which is below is like that which is above and that which is above is like that which is below to do ye miracles of one only thing.
+3: And as all things have been and arose from one by ye mediation of one: so all things have their birth from this one thing by adaptation.
+
+If you are asked to translate into Chinese, you need to submit the translated content in the following format:
+1: 这是真的，没有任何虚妄，是确定的，最真实的：
+2: 上如其下，下如其上，以此来展现“一”的奇迹。
+3: 万物皆来自“一”的沉思，万物在“一”的安排下诞生。
+"""
