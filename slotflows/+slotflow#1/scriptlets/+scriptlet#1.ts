3e0594bbdd4eb60f13247abd4b4db305351325bc
shaun <beingswu@gmail.com>
2025-06-02 12:34:52 +0800

    update video translate

---
+++ slotflows/+slotflow#1/scriptlets/+scriptlet#1.ts
@@ -0,0 +1,23 @@
+//#region generated meta
+import type { Context } from "@oomol/types/oocana";
+type Inputs = {
+    args: any;
+};
+type Outputs = {
+    destination_folder: string | null;
+    source: "en" | "cn" | "ja" | "fr" | "ru" | "de";
+    target: "en" | "cn" | "ja" | "fr" | "ru" | "de";
+};
+//#endregion
+
+export default async function(
+    params: Inputs,
+    context: Context<Inputs, Outputs>
+): Promise<Partial<Outputs> | undefined | void> {
+    const {args} = params;
+    const destination_folder = args.destination_folder;
+    const source = args.source;
+    const target = args.target;
+
+    return { destination_folder: destination_folder, source: source, target: target };
+};
