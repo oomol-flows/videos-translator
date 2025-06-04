fbac0a0fda1f3711c0b0fd2758dad8982ffe6499
shaun <beingswu@gmail.com>
2025-06-04 18:39:35 +0800

    Update videos translater (#15)

---
+++ tasks/video-filter/main.ts
@@ -0,0 +1,23 @@
+//#region generated meta
+import type { Context } from "@oomol/types/oocana";
+type Inputs = {
+    file_paths: string[];
+};
+type Outputs = {
+    array: any[];
+};
+//#endregion
+
+export default async function(
+    params: Inputs,
+    context: Context<Inputs, Outputs>
+): Promise<Partial<Outputs> | undefined | void> {
+
+    const videoExtensions = ['.mp4', '.avi', '.mov', '.wmv', '.flv', '.mkv'];
+    const videoFiles = params.file_paths.filter(filePath => {
+        const lowerCaseFilePath = filePath.toLowerCase();
+        return videoExtensions.some(ext => lowerCaseFilePath.endsWith(ext));
+    });
+
+    return { array: videoFiles };
+};
